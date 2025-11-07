a982429ee390b670c430a0a8c00859a35b164788
Shvigma <luckinmatvej2@gmail.com>
2025-11-07 17:37:49 +0300

    3rd rework + CW

--- HW/HW3/c#/Program.cs
+++ HW/HW3/c#/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using System.Threading;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System;
 using System.Linq.Expressions;
 using System.Reflection.Emit;
 using System.Security.Permissions;
@@ -74,6 +79,7 @@ class Polynomia{
                                 else{z+=" - "+ Math.Abs(coefs[i]) +"x^"+ i;}
                             }
 
+
                             else
                             {
                                 if(i!=coefs.Length-1 && coefs[i+1]<0){z+=" + " + coefs[i] +"x^"+ i + " - ";}
@@ -86,33 +92,66 @@ class Polynomia{
         }
         return z;
     }
+    public static Polynomia operator+(Polynomia obj1, Polynomia obj2){
+        int k =  obj1.coefs.Length > obj2.coefs.Length ?  obj1.coefs.Length : obj2.coefs.Length;
+        double[] coefs= new double[3];
+        Array.Resize(ref coefs, k);
+        for(int i =0; i<k; i++){
+            if (obj1.coefs.Length>i){
+                if (obj2.coefs.Length>i){
+                    coefs[i]= obj2.coefs[i]+obj1.coefs[i];
+
+
+                }
+                else{
+                    coefs[i]=obj1.coefs[i];
+                }
+            }
+            else if(obj2.coefs.Length>i){
+                coefs[i]=obj2.coefs[i];
+            }
+        }
+
+
+        return new Polynomia(coefs);
+    }
 } 
 class Programm
 {
     static void Main(string[] args)
     {   
-        double[] coeffs;
-        coeffs =  [1.0, 0.0, 2.0 ];
+        double[] coeffs = new double[] {1.0, 0.0, 2.0 };
         Polynomia p = new Polynomia(coeffs);
         Console.WriteLine("При дефолтных значениях [1.0, 0.0, 2.0]");
         Console.WriteLine(p);
 
-        coeffs = [];
+        double[] coffs = new double[] {};
         Console.WriteLine("Введите сколько будет коэффицентов");
         int q= int.Parse(Console.ReadLine());
         if (q >0){
         for (int i=0; i!=q; i++)
         {
-            Console.WriteLine("Введите элемент №" + i);
-            Array.Resize(ref coeffs, coeffs.Length + 1);
-            coeffs[coeffs.Length - 1] = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите элемент №" + (i+1));
+            Array.Resize(ref coffs, coffs.Length + 1);
+            coffs[coffs.Length - 1] = Convert.ToDouble(Console.ReadLine());
+
+
         }
-        Polynomia z = new Polynomia(coeffs);
+        Polynomia z = new Polynomia(coffs);
         Console.WriteLine("При значениях введеных вами:");
-        Console.WriteLine(z);}
+        Console.WriteLine(z);
+        Polynomia l = p + z;
+        Console.WriteLine("Если сложить 2 полученных многочлена, то получится");
+        Console.WriteLine(l);
+        }
         else 
         {
             Console.WriteLine("Вы ввели " + q +" при таком колличестве коефицентов программа выводит пустоту, пожалуйста вводите числа > 0");
         }
+
+
     }
 }
+
+
+
